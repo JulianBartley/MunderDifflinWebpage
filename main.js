ddb3d0783a73cb7ec6db8ba4cd055e2e9a642c36
JulianBartley <juliangbartley@gmail.com>
2019-11-12 06:01:28 -0500

    added some JS

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-$("h1").lettering();
+// console.log('Julian')
+//
+//
+//
+// function Scroll(){
+//   var top = document.getElementById('grid-item1');
+//   var ypos = window.pageYOffset;
+//     if(ypos > 587) {
+//       top.style.height = "80px";
+//     }
+//     else {
+//       top.style.height="150px";
+//     }
+// }
+//   window.addEventListener("scroll",Scroll);
